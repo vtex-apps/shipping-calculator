3d7a549ad0a4d3e0f5a385aa7b5408952fe453f0
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2019-10-31 14:11:32 -0300

    Fix CI test

--- react/__mocks__/vtex.styleguide.tsx
+++ react/__mocks__/vtex.styleguide.tsx
@@ -1,6 +1,7 @@
 import Button from '@vtex/styleguide/lib/Button'
+import ButtonPlain from '@vtex/styleguide/lib/ButtonPlain'
 import Input from '@vtex/styleguide/lib/Input'
 import Tag from '@vtex/styleguide/lib/Tag'
 import Radio from '@vtex/styleguide/lib/Radio'
 
-export { Button, Input, Tag, Radio }
+export { Button, ButtonPlain, Input, Tag, Radio }
