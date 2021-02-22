3d1c1f25abfe3bf0692c400a97a45fa9c1362e38
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-02-22 09:54:14 -0300

    Use ShippingForm component from checkout-shipping

--- react/ShippingCalculator.tsx
+++ react/ShippingCalculator.tsx
@@ -1,41 +1,17 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState } from 'react'
 import { OrderShipping } from 'vtex.order-shipping'
-import {
-  ShippingHeader,
-  ShippingOptionList,
-  NewAddressForm,
-} from 'vtex.checkout-shipping'
-import { AddressContext } from 'vtex.address-context'
+import { ShippingForm } from 'vtex.checkout-shipping'
 import { FormattedMessage } from 'react-intl'
 import { ButtonPlain } from 'vtex.styleguide'
 
 const { useOrderShipping } = OrderShipping
-const { useAddressContext } = AddressContext
 
 const ShippingCalculator: React.VFC = () => {
-  const {
-    deliveryOptions,
-    pickupOptions,
-    selectDeliveryOption,
-    selectPickupOption,
-    selectedAddress,
-    insertAddress,
-  } = useOrderShipping()
-  const { address, setAddress } = useAddressContext()
-  const [editingAddress, setEditingAddress] = useState(!address?.postalCode)
+  const { selectedAddress } = useOrderShipping()
   const shouldInitiallyShowShippingEstimate = !!selectedAddress?.postalCode
 
   const [showShippingEstimate, setShowShippingEstimate] = useState(
     shouldInitiallyShowShippingEstimate
-  )
-
-  const handleAddressSuccess = useCallback(
-    createdAddress => {
-      return insertAddress(createdAddress).then(() => {
-        setEditingAddress(false)
-      })
-    },
-    [insertAddress]
   )
 
   if (!showShippingEstimate) {
@@ -51,27 +27,7 @@ const ShippingCalculator: React.VFC = () => {
     )
   }
 
-  return (
-    <>
-      <ShippingHeader
-        onEditAddress={() => {
-          setEditingAddress(true)
-          setAddress(null)
-        }}
-      />
-
-      {!editingAddress ? (
-        <ShippingOptionList
-          deliveryOptions={deliveryOptions}
-          pickupOptions={pickupOptions}
-          onDeliveryOptionSelected={selectDeliveryOption}
-          onPickupOptionSelected={selectPickupOption}
-        />
-      ) : (
-        <NewAddressForm onAddressCreated={handleAddressSuccess} />
-      )}
-    </>
-  )
+  return <ShippingForm />
 }
 
 export default ShippingCalculator
